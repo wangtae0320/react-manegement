efc65c5e1943f73748bc20b48a844f6526651017
wangtae0320 <85270112+wangtae0320@users.noreply.github.com>
2021-11-11 01:03:46 +0900

    props로 데이터 전송 및 map 사용

--- src/App.js
+++ src/App.js
@@ -2,19 +2,50 @@ import React, { Component } from 'react';
 import Customer from './component/Customer';
 import './App.css';
 
-const customer = {
-  'name' : '김기명',
-  'birthday' : '910320'
-}
+const customer = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '홍길동',
+    'birthday' : '910320',
+    'gender' : '남자'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '영미',
+    'birthday' : '921220',
+    'gender' : '여자'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '철수',
+    'birthday' : '930410',
+    'gender' : '남자'
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-      />
-    );
+      <div>
+        {
+          customer.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+              />
+            )
+          })
+        }
+      </div>
+    )
   }
 }
 
--- src/component/Customer.js
+++ src/component/Customer.js
@@ -4,8 +4,30 @@ class Customer extends React.Component {
     render() {
         return(
             <div>
-                <h2>{this.props.name}</h2>
-                <h2>{this.props.birthday}</h2>
+                <CustmoerProfile id={this.props.id} image={this.props.image} name={this.props.name} />
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender}/>
+            </div>
+        )
+    }
+}
+
+class CustmoerProfile extends React.Component {
+    render () {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render () {
+        return  (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
             </div>
         )
     }
