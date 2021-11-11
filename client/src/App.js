4d99bdddd0f76a0ef39ceff78b007776ce412026
wangtae0320 <85270112+wangtae0320@users.noreply.github.com>
2021-11-11 16:49:28 +0900

    lifecycle 이해 및 loading 화면 처리

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,6 +18,9 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
@@ -25,10 +29,12 @@ const styles = theme => ({
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -38,6 +44,12 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;  
+  }
+
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 })
   }
 
   render() {
@@ -57,7 +69,13 @@ class App extends Component {
           <TableBody>
             { this.state.customers ? this.state.customers.map(c => { 
               return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} />)}) 
-            : ""}
+            : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed} />                
+                  </TableCell>
+              </TableRow>
+            }
           </TableBody>
           </Table>
       </Paper>
