05c553ab309ad96e4a1cf90bdcdd65ef5dfdec45
wangtae0320 <85270112+wangtae0320@users.noreply.github.com>
2021-11-11 18:15:52 +0900

    mysql DB와 연동

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-Parser')
 const app = express();
@@ -6,30 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+
+connection.connect;
+
 app.get('/api/customers', (req, res) =>{
-    res.send([
-        {
-          'id' : 1,
-          'image' : 'https://placeimg.com/64/64/1',
-          'name' : '홍길동',
-          'birthday' : '910320',
-          'gender' : '남자'
-        },
-        {
-          'id' : 2,
-          'image' : 'https://placeimg.com/64/64/2',
-          'name' : '영미',
-          'birthday' : '921220',
-          'gender' : '여자'
-        },
-        {
-          'id' : 3,
-          'image' : 'https://placeimg.com/64/64/3',
-          'name' : '철수',
-          'birthday' : '930410',
-          'gender' : '남자'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-      ])
+    )
 });
 
 app.listen(port,() =>{ console.log(`Listening on port ${port}` )});
