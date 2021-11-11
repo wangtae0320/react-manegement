d4366cffc20602636b0443ba2656781eee25a2b6
wangtae0320 <85270112+wangtae0320@users.noreply.github.com>
2021-11-11 16:16:47 +0900

    add Get Customers RestApi Moduele

--- client/src/App.js
+++ client/src/App.js
@@ -20,31 +20,26 @@ const styles = theme => ({
   }
 })
 
-const customer = [
-  {
-    'id' : 1,
-    'image' : 'https://placeimg.com/64/64/1',
-    'name' : '홍길동',
-    'birthday' : '910320',
-    'gender' : '남자'
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name' : '영미',
-    'birthday' : '921220',
-    'gender' : '여자'
-  },
-  {
-    'id' : 3,
-    'image' : 'https://placeimg.com/64/64/3',
-    'name' : '철수',
-    'birthday' : '930410',
-    'gender' : '남자'
-  }
-]
+
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;  
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -60,7 +55,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            { customer.map(c => { return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} />)}) }
+            { this.state.customers ? this.state.customers.map(c => { 
+              return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} />)}) 
+            : ""}
           </TableBody>
           </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) =>{
-    res.send({message: 'Hello Express!'});npm
+app.get('/api/customers', (req, res) =>{
+    res.send([
+        {
+          'id' : 1,
+          'image' : 'https://placeimg.com/64/64/1',
+          'name' : '홍길동',
+          'birthday' : '910320',
+          'gender' : '남자'
+        },
+        {
+          'id' : 2,
+          'image' : 'https://placeimg.com/64/64/2',
+          'name' : '영미',
+          'birthday' : '921220',
+          'gender' : '여자'
+        },
+        {
+          'id' : 3,
+          'image' : 'https://placeimg.com/64/64/3',
+          'name' : '철수',
+          'birthday' : '930410',
+          'gender' : '남자'
+        }
+      ])
 });
 
 app.listen(port,() =>{ console.log(`Listening on port ${port}` )});
